1ba76f930e000b067817ebc4bc82120f0caf64f8
Hugo Le Moine <hugo.le-moine@outlook.fr>
2023-07-24 21:16:42 +0200

    Updated packages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,10 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-/** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Hugo Le Moine',
   tagline: 'Data Scientist',
@@ -76,6 +74,7 @@ const config = {
         disableInDev: false,
       },
     ],
+    'docusaurus-plugin-sass'
   ],
 
   themeConfig:
