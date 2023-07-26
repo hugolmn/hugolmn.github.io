7024ba6719f66d48b01f6e933ae569a0559dab89
Hugo Le Moine <hugo.le-moine@outlook.fr>
2023-07-26 20:30:21 +0200

    Added GA4

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,6 +60,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-PM17LZGEZ1',
+          anonymizeIP: true,
+        },
       },
     ],
   ],
