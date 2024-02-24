7352aaf7d8d2eb34847f7f09f95ccccd8b08b2e6
Hugo Le Moine <hugo.le-moine@outlook.fr>
2024-02-24 17:13:07 +0100

    Component for buy me a coffee

---
+++ src/components/BuyMeACoffee/index.js
@@ -0,0 +1,7 @@
+export default function BuyMeACoffee() {
+    return (
+        <a href="https://www.buymeacoffee.com/hugolmn" target="_blank">
+            <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=hugolmn&button_colour=3578e5&font_colour=ffffff&font_family=Cookie&outline_colour=ffffff&coffee_colour=FFDD00" />
+        </a>
+    )
+}
